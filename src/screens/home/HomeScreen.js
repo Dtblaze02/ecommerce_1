686e1a77b1280706d06483e55bb980b4c859c3c5
Abdulkareem Shehu <shehuabdulkareem01@gmail.com>
2024-02-14 11:57:21 +0100

    modified HomeScreen

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -5,7 +5,7 @@ const HomeScreen = () => {
     return (
         <View>
             <Text>
-                This is my HomeScreen
+                This is my HomeScreen Bigmanforlife.
             </Text>
         </View>
     )
