f206270ed8c393b6c0dce135e2e20932b218fc37
jankaaudi <jankasaudi@gmail.com>
2024-02-15 03:50:49 -0800

    added my name

--- src/screens/detail/DetailScreen.js
+++ src/screens/detail/DetailScreen.js
@@ -5,7 +5,7 @@ const DetailScreen = ()=>{
     return (
         <View>
             <Text>
-                This is product details screen
+                This is product details screen by Mr. jankasa david michael
             </Text>
         </View>
     )
