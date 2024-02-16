51e14b7d4cc0b39e1c0a8ab8c1f88c1f6c6e8757
Maureentex <160138131+Maureentex@users.noreply.github.com>
2024-02-16 15:47:53 +0100

    modified

--- src/products/Products.js
+++ src/products/Products.js
@@ -322,6 +322,15 @@ export const products = [
   quantity: '20',
   description: '',
   discount:''
+},
+{
+  id: 63,
+  name: 'kids gown-1.jpg',
+  price: '$56.99',
+  image: require('../../assets/images/kids gown-1.jpg'),
+  category:'fashion',
+  quantity: '35',
+  description: '',
+  discount:''
 }
-
 ];
