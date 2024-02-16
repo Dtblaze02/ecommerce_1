ff46a4aa41bb333a98c58d371e08b75af9941350
Adj1zgr8 <coolaidejay@gmail.com>
2024-02-16 10:38:21 +0100

    added jacket again corrected errors

--- src/products/Products.js
+++ src/products/Products.js
@@ -1,4 +1,8 @@
 export const products = [
+  
+    // Add more products as needed
+   
+   
     {
       id: 1,
       name: 'Bag 1',
@@ -26,12 +30,51 @@ export const products = [
         price: '$19.99',
         image: require('../../assets/images/watch-1.png'),
       },
+
+
+      {
+        id: 30,
+        name: 'Product 30',
+        price: '$24.99',
+        image: require('../../assets/images/belt-1.png'),
+      },
+
+      {
       id: 5,
         name: 'Product 5',
         price: '$50.99',
-        image: require('../../assets/images/footwears.png'),
+        image: require('../../assets/images/footwear.png'),
+      },
       {
-
-      }
+        id: 6,
+          name: 'Product 6',
+          price: '$10.99',
+          image: require('../../assets/images/watch-6.jpg'),
+        },
+        {
+          id: 7,
+            name: 'Product 7',
+            price: '$150.99',
+            image: require('../../assets/images/w-shoes_1.jpg'),
+          },
+            
+                {
+                  id: 19,
+                  name: 'Product 19',
+                  price: '$34.99',
+                  image: require('../../assets/images/kinomo-1.jpg'),
+              },
+               {
+      id: 9,
+      name: 'jacket1',
+      price: '$19.99',
+      image: require('../../assets/images/jacket.jpg'),
+    },
+    {
+      id: 10,
+      name: 'jacket2',
+      price: '$19.99',
+      image: require('../../assets/images/jacket.jpg'),
+    },
     // Add more products as needed
   ];
