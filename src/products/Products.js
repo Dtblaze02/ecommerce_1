8e135339154bc6a584691193de31067e08af0cbd
Dtblaze02 <44786869+Dtblaze02@users.noreply.github.com>
2024-02-16 14:52:19 +0100

    Update Products.js

--- src/products/Products.js
+++ src/products/Products.js
@@ -53,8 +53,6 @@ export const products = [
         description: '',
         discount:''
       },
-
-
       {
         id: 30,
         name: 'Product 30',
@@ -319,7 +317,7 @@ export const products = [
   id: 60,
   name: 'w-shoes_1.png',
   price: '$16.99',
-  image: require('../../assets/images/w-shoes_1.png'),
+  image: require('../../assets/images/w-shoes_1.jpg'),
   category:'fashion',
   quantity: '20',
   description: '',
