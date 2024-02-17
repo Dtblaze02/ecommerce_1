fc75ff4bf2329c9dbb629962521729f8578cd571
Daniel UCHEIBE <ducheibe@gmail.com>
2024-02-17 06:38:38 +0100

    thank you

--- src/products/Products.js
+++ src/products/Products.js
@@ -52,25 +52,22 @@ export const products = [
         category: 'fashion',
         quantity: '6',
         description: '',
-<<<<<<< HEAD
         discount:''
       },
       {
         id: 6,
         name: 'Product 6',
-=======
+
         discount: ''
     }, {
         id: 11,
-        name: 'Product 11',
->>>>>>> 5a1777226e168138242c30725bc23d026da17226
+        name: 'Product 11'
         price: '$19.99',
         image: require('../../assets/images/Teemespice_Slippers.jpg'),
         category: 'fashion',
         quantity: '20',
         description: '',
         discount:''
-<<<<<<< HEAD
       },
       {
         id: 7,
@@ -85,46 +82,38 @@ export const products = [
       {
         id: 8,
         name: 'Product 8',
-=======
       }, {
         id: 30,
         name: 'Product 30',
->>>>>>> 5a1777226e168138242c30725bc23d026da17226
         price: '$24.99',
         image: require('../../assets/images/belt-1.png'),
         category:'fashion',
         quantity: '10',
         description: '',
-<<<<<<< HEAD
         discount: ''
     }, {
         id: 9,
         name: 'Product 9',
-=======
         discount:''
       },
       {         
       id: 5,
         name: 'Product 5',
->>>>>>> 5a1777226e168138242c30725bc23d026da17226
         price: '$50.99',
         image: require('../../assets/images/footwear.png'),
         category:'fashion',
         quantity: '34',
         description: '',
-<<<<<<< HEAD
         discount: ''
 
     }, {
         id: 10,
           name: 'Product 10',
-=======
         discount:''
       },
       {
         id: 6,
           name: 'Product 6',
->>>>>>> 5a1777226e168138242c30725bc23d026da17226
           price: '$10.99',
           image: require('../../assets/images/watch-6.jpg'),
           category:'electronics',
@@ -684,18 +673,16 @@ export const products = [
         description: '',
         discount: ''
     }, {
-      id: 911,
+      id: 711,
       name: 'Tecno Phone',
       price: '$64.99',
-      image: require('../../assets/images/tecno-camon.jpg'),
+      image: require('../../assets/images/tecno-camon-16.png'),
       category: 'electronics',
       quantity: '75',
       description: 'available',
       discount: ''
   }, 
-    
-    
-    
+
     {
         id: 12,
         name: 'Product 12',
